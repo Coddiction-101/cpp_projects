5e3ff951d8074d27d5be56f8bc1cf09a0527e45d
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-02-22 17:06:01 +0530

    Update code.cpp

--- cpp-project-ideas/Tools-and-Utilities/Terminal-Text-Editor/code.cpp
+++ cpp-project-ideas/Tools-and-Utilities/Terminal-Text-Editor/code.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
 
+int main()
+{
+   cout << "Press any key (ESC to quit): \n\n";
+
+   while (true)
+   {
+      if (_kbhit())
+      {
+         int ch = _getch();
+
+         if (ch == 27)
+         {
+            cout << "\nExisting...\n";
+            break;
+         }
+
+         cout << "you pressed: " << ch << " (" << (char)ch << ")\n";
+      }
+   }
+   return 0;
+}
