69e26d5d881acc60c29e1bff6354f14da09b62b1
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-02-22 17:33:19 +0530

    Update code.cpp

--- cpp-project-ideas/Tools-and-Utilities/Terminal-Text-Editor/code.cpp
+++ cpp-project-ideas/Tools-and-Utilities/Terminal-Text-Editor/code.cpp
@@ -2,24 +2,63 @@
 #include <conio.h>
 using namespace std;
 
+void clearScreen()
+{
+   cout << "\033[2J\033[H"; // ✅ No ] at the end
+}
+
+void moveCursor(int row, int col)
+{
+   cout << "\033[" << row << ";" << col << "H";
+}
+
 int main()
 {
-   cout << "Press any key (ESC to quit): \n\n";
+   clearScreen();
+   int cursorRow = 10;
+   int cursorCol = 20;
 
+   cout << "Use array key to move. ESC to quit.\n";
    while (true)
    {
+      moveCursor(cursorRow, cursorCol);
+      cout << "*" << flush;
+
       if (_kbhit())
       {
          int ch = _getch();
 
          if (ch == 27)
-         {
-            cout << "\nExisting...\n";
             break;
-         }
 
-         cout << "you pressed: " << ch << " (" << (char)ch << ")\n";
+         if (ch == 0 || ch == 224)
+         {
+            ch = _getch();
+
+            moveCursor(cursorRow, cursorCol);
+            cout << " ";
+
+            if (ch == 72)
+               cursorRow--; // UP
+            if (ch == 80)
+               cursorRow++; // DOWN
+            if (ch == 75)
+               cursorCol--; // Left
+            if (ch == 77)
+               cursorCol++; // Right
+
+            // Bounderies
+            if (cursorRow < 2)
+               cursorRow = 2;
+            if (cursorRow > 20)
+               cursorRow = 20;
+            if (cursorCol < 1)
+               cursorCol = 1;
+            if (cursorCol > 40)
+               cursorCol = 40;
+         }
       }
    }
+   clearScreen();
    return 0;
 }
